992bc710f2f7a7e07f6b730e308ab0fec16fd644
kacperlokaj <kacper.lokaj@interia.pl>
2022-11-24 17:28:30 +0100

    wejsciowka

---
+++ Lab6/wejsciowka.py
@@ -0,0 +1,5 @@
+"Grupa 1. Użytkownik podaje 5 liczb. Zapisz do listy tylko liczby nieparzyste."
+b = int(input("podaj ilosc liczb"))
+for i in range(b):
+    g=input("Podaj liczby:")
+    a.append(g)
